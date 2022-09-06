74f807d9e4295dff526921a70cb6f45c3f080657
DaveyJH <daveyj.dh@googlemail.com>
2022-09-06 06:21:55 +0100

    feat: Add Home model | fix #29

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Home(models.Model):
+    """Home model
+
+    ---
+    Attributes:
+        content : introductory text to the site
+        address : physical address and contact details
+    """
+
+    content = models.TextField()
+    address = models.TextField()
+
+    def __str__(self):
+        """Returns `content`"""
+        return self.content
