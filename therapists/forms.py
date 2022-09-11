538eb8f295b0ab6a0587cf8155ddf9415aad095d
DaveyJH <daveyj.dh@googlemail.com>
2022-09-11 14:29:51 +0100

    feat: Add add_therapist to frontend for admin
    Add form, view and template for adding therapists while logged in as
    admin. Remaining characters displayed via JavaScript.
    
    fix #47

---
+++ therapists/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Therapist
+from cloudinary.models import CloudinaryField
+
+
+class CreateTherapistForm(forms.ModelForm):
+    """
+    Form to edit and delete menus
+    """
+    class Meta:
+        """
+        Define model, form fields and label
+        """
+        model = Therapist
+        fields = [
+            'first_name',
+            'last_name',
+            'bio',
+            'image',
+            'image_alt_text',
+            'hourly_rate',
+        ]
+
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    bio = forms.CharField(
+        max_length=240, widget=forms.Textarea(attrs={'rows': 4}),
+    )
+    image = CloudinaryField('image')
+    image_alt_text = forms.CharField(
+        max_length=80, label="Accessibility description of image"
+    )
+    hourly_rate = forms.IntegerField(
+        max_value=100, label="Hourly rate (whole numbers only)"
+    )
--- therapists/urls.py
+++ therapists/urls.py
@@ -3,4 +3,19 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.TherapistsView.as_view(), name='therapists'),
+    path(
+        'add_therapist/',
+        views.AddTherapistView.as_view(),
+        name='add_therapist'
+    ),
+    path(
+        'delete_therapist/',
+        views.TherapistsView.as_view(),
+        name='delete_therapist'
+    ),
+    path(
+        'edit_therapist/',
+        views.TherapistsView.as_view(),
+        name='edit_therapist'
+    ),
 ]
