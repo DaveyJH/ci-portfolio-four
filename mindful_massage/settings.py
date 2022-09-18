26e31d58eb997ba022bbdc42f1e3574cae941a17
DaveyJH <daveyj.dh@googlemail.com>
2022-09-18 12:19:38 +0100

    build: Prepare for Render deployment

--- mindful_massage/settings.py
+++ mindful_massage/settings.py
@@ -34,6 +34,11 @@ ALLOWED_HOSTS = [
     HOST,
 ]
 
+# Add render URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
