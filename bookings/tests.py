5865f8e75921fc8e469c91cb4e3143c9ef5ea7d8
DaveyJH <daveyj.dh@googlemail.com>
2022-09-20 04:08:23 +0100

    tests: home and bookings tests added

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from .models import BookingInfo
+from home.models import Home
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        """creates a home object"""
+        self.home = Home(content="Some content", address="an address")
+        self.home.save()
+
+    def test_booking_info_template(self):
+        """Tests bookings page renders when all necessary data is present"""
+        self.booking_info = BookingInfo(
+            message="A message",
+            contact_info="0761234567",
+            open_times={
+                "Monday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+                "Tuesday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+                "Wednesday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+                "Thursday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+                "Friday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+                "Saturday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+                "Sunday": {
+                    "open": "0900",
+                    "close": "1400",
+                },
+            }
+        )
+        self.booking_info.save()
+        response = self.client.get('/bookings/book/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/booking_info.html')
+        self.assertTemplateUsed(response, 'base.html')
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_404(self):
+        response = self.client.get('/this_doesnt_exist/')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, '404.html')
+        self.assertTemplateUsed(response, 'base.html')
